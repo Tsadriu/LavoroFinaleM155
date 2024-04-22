daebad197f8379509161bc918c4a309e7a295013
Tsadriu <40073056+Tsadriu@users.noreply.github.com>
2024-04-22 15:53:58 +0200

    Implemented slider from 0 to 255

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,9 +32,14 @@
             ledLabel = new Label();
             ledComboBox = new ComboBox();
             groupBox2 = new GroupBox();
+            ledValueLabel = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            ledTrackBar = new TrackBar();
             ledStateCheckBox = new CheckBox();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)ledTrackBar).BeginInit();
             SuspendLayout();
             // 
             // groupBox1
@@ -68,13 +73,54 @@
             // 
             // groupBox2
             // 
+            groupBox2.Controls.Add(ledValueLabel);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Controls.Add(label1);
+            groupBox2.Controls.Add(ledTrackBar);
             groupBox2.Controls.Add(ledStateCheckBox);
             groupBox2.Location = new Point(12, 104);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(252, 184);
+            groupBox2.Size = new Size(260, 184);
             groupBox2.TabIndex = 1;
             groupBox2.TabStop = false;
             groupBox2.Text = "Led properties";
+            // 
+            // ledValueLabel
+            // 
+            ledValueLabel.AutoSize = true;
+            ledValueLabel.Location = new Point(111, 92);
+            ledValueLabel.Name = "ledValueLabel";
+            ledValueLabel.Size = new Size(35, 15);
+            ledValueLabel.TabIndex = 4;
+            ledValueLabel.Text = "100%";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(229, 71);
+            label2.Name = "label2";
+            label2.Size = new Size(25, 15);
+            label2.TabIndex = 3;
+            label2.Text = "255";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 71);
+            label1.Name = "label1";
+            label1.Size = new Size(13, 15);
+            label1.TabIndex = 2;
+            label1.Text = "0";
+            // 
+            // ledTrackBar
+            // 
+            ledTrackBar.Location = new Point(19, 62);
+            ledTrackBar.Maximum = 255;
+            ledTrackBar.Name = "ledTrackBar";
+            ledTrackBar.Size = new Size(208, 45);
+            ledTrackBar.TabIndex = 1;
+            ledTrackBar.Value = 255;
+            ledTrackBar.Scroll += trackBar1_Scroll;
             // 
             // ledStateCheckBox
             // 
@@ -99,6 +145,7 @@
             groupBox1.PerformLayout();
             groupBox2.ResumeLayout(false);
             groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)ledTrackBar).EndInit();
             ResumeLayout(false);
         }
 
@@ -109,5 +156,9 @@
         private Label ledLabel;
         private GroupBox groupBox2;
         private CheckBox ledStateCheckBox;
+        private TrackBar ledTrackBar;
+        private Label label1;
+        private Label ledValueLabel;
+        private Label label2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,16 @@ namespace LavoroFinaleM155
         {
             InitializeComponent();
         }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (ledTrackBar.Value > 0)
+            {
+                ledValueLabel.Text = ((float)(ledTrackBar.Value / 255.0f) * 100.0f).ToString("#") + "%";
+                return;
+            }
+
+            ledValueLabel.Text = "0%";
+        }
     }
 }
